d1390dc64758b585d52a6810d5a9114eb2706f51
柏崧張 <baisongzhang@baisongzhangdeMacBook-Pro.local>
2023-02-04 14:48:26 +0800

    fix: open actions permissions for writing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.scss"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import gsap from 'gsap';
 
-// Try4
+// Try5
 // Scene
 const scene  = new THREE.Scene();
 
